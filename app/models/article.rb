a057f4455288b8d1ddbc82f88e179773095fe35c
fernandojerez <fernandojerezy@gmail.com>
2023-07-18 11:29:54 -0400

    se configura article y rutas

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,8 @@
 class Article < ApplicationRecord
   has_one_attached :foto
+  #app/models/article.rb
+  has_many :comments, dependent: :destroy
+  has_many :reactions, dependent: :destroy
+  has_many :users, through: :reactions #or comments doesn't matter
+  
 end
